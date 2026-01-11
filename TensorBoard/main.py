823f3b771e03f0553717637bc98bf85dd9dfd3d5
Tourde <2230940897@qq.com>
2026-01-11 10:26:24 +0800

    删除没用的import

--- TensorBoard/main.py
+++ TensorBoard/main.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
-import tensorboard
 from torch.utils.tensorboard import SummaryWriter
 
 class Net(nn.Module):
